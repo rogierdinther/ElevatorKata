81bbfb84c0b03f64e66a11e096a481785b8fecde
Rogier van Dinther <rogier.vandinther@ns.nl>
2024-04-23 10:31:52 +0200

    DUmmy test

--- src/test/kotlin/DummyTest.kt
+++ src/test/kotlin/DummyTest.kt
@@ -1,2 +1,11 @@
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+// ./gradlew test --continuous --quiet
+
 class DummyTest {
+    @Test
+    fun dummyTest() {
+        assertTrue(true)
+    }
 }
